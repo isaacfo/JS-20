bdf9093ab81948f3c96b5f8d328a1674ff1fee33
isaacfonsecalopez <isaacfo1010@gmail.com>
2020-02-12 18:03:16 -0500

    set local storage for movies and seats

--- MOVIE-SEAT-BOOKING/script.js
+++ MOVIE-SEAT-BOOKING/script.js
@@ -8,10 +8,22 @@ const movieSelect = document.getElementById('movie');
 // + makes string into a number
 let ticketPrice = +movieSelect.value;
 
+// Save selected moive index and price
+function setMovieData(movieIndex, moviePrice) {
+  localStorage.setItem('selectedMovieIndex', movieIndex);
+  localStorage.setItem('selectedMoviePrice', moviePrice);
+}
+
 // update total and count
 function updateSelectedCount() {
   // put all seats that are selected into a nodelist which is an array
   const selectedSeats = document.querySelectorAll('.row .seat.selected');
+
+  // copying orginal array of selectedSeats into seatsIndex with ... spread operator then map through it to return array of the index of the seats selected
+  const seatsIndex = [...selectedSeats].map(seat => [...seats].indexOf(seat));
+
+  // have to wrap seatsIndex in JSON .stringify cuz seatsIndex is an array
+  localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
 
   const selectedSeatsCount = selectedSeats.length;
 
@@ -21,6 +33,7 @@ function updateSelectedCount() {
 // movie selct event to change prices based on movie
 movieSelect.addEventListener('change', e => {
   ticketPrice = +e.target.value;
+  setMovieData(e.target.selectedIndex, e.target.value);
   updateSelectedCount();
 });
 
