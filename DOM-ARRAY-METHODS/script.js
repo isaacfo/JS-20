b5d0ecbb811d7f402578b753e684ff39c38f8856
isaacfonsecalopez <isaacfo1010@gmail.com>
2020-02-21 14:55:10 -0500

    added double money function

--- DOM-ARRAY-METHODS/script.js
+++ DOM-ARRAY-METHODS/script.js
@@ -1,8 +1,8 @@
-// Five most commmon array methods used: forEach(), map(), sort(), filter(), reduce()
+// Five most commmon array methods used: forEach(), map(), sort(), filter(), reduce(), called high order array methods
 
 const main = document.getElementById('main');
 const addUserBtn = document.getElementById('add-user');
-const doubleBTN = document.getElementById('double');
+const doubleBtn = document.getElementById('double');
 const showMillionaireBtn = document.getElementById('show-millionaires');
 const sortBtn = document.getElementById('sort');
 const calculateWealthBtn = document.getElementById('calculate-wealth');
@@ -26,10 +26,47 @@ async function getRandomUser() {
     money: Math.floor(Math.random() * 1000000)
   };
 
-  console.log(newUser);
+  addData(newUser);
+}
+
+// Double everyones money
+function doubleMoney() {
+  // map() returns an array ... copies whats in user object and returns it
+  data = data.map(user => {
+    return { ...user, money: user.money * 2 };
+  });
+  updateDOM();
 }
 
 // Add the new obj to data arr
 function addData(obj) {
   data.push(obj);
+
+  updateDOM();
 }
+
+// Update DOM
+// if nothing is passed into the updateDOM the default of == data will show up
+function updateDOM(providedData = data) {
+  // Clear main div
+  main.innerHTML = '<h2><strong>Person</strong>Wealth</h2>';
+
+  providedData.forEach(item => {
+    const element = document.createElement('div');
+    // adds a class to the new div created
+    element.classList.add('person');
+    element.innerHTML = `<strong>${item.name}</strong> ${formatMoney(
+      item.money
+    )}`;
+    main.appendChild(element);
+  });
+}
+
+//  Format number as money
+function formatMoney(number) {
+  return '$' + number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+}
+
+// Event listeners
+addUserBtn.addEventListener('click', getRandomUser);
+doubleBtn.addEventListener('click', doubleMoney);
