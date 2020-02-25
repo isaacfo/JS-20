7a24cefe054f9670c30a9949b521f6c0afdb6199
isaacfonsecalopez <isaacfo1010@gmail.com>
2020-02-25 14:36:18 -0500

    added functions for displaying letter when guessed

---
+++ HANGMAN/script.js
@@ -0,0 +1,40 @@
+const wordEl = document.getElementById('word');
+const wrongLettersEl = document.getElementById('wrong-letters');
+const playAgainBtn = document.getElementById('play-again');
+const popup = document.getElementById('popup-container');
+const notification = document.getElementById('notification-container');
+const finalMessage = document.getElementById('final-message');
+
+const figureParts = document.querySelectorAll('.figure-part');
+
+const words = ['application', 'programming', 'interface', 'wizard'];
+
+let selectedWord = words[Math.floor(Math.random() * words.length)];
+
+const correctLetters = ['w', 'i', 'z', 'a', 'r', 'd'];
+const wrongLetters = [];
+
+// Show hidden letter if its present in the word
+function displayWord() {
+  wordEl.innerHTML = `
+    ${selectedWord
+      .split('')
+      .map(
+        letter =>
+          `<span class="letter">
+        ${correctLetters.includes(letter) ? letter : ''}
+        </span>
+`
+      )
+      .join('')}
+    `;
+  //  replace new line character with a empty space
+  const innerWord = wordEl.innerText.replace(/\n/g, '');
+
+  if (innerWord === selectedWord) {
+    finalMessage.innerText = 'Congratulations! You won!';
+    popup.style.display = 'flex';
+  }
+}
+
+displayWord();
