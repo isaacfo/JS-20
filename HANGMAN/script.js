bb8a7d921b4b190e013581b8d90bcfb29849dede
isaacfonsecalopez <isaacfo1010@gmail.com>
2020-02-25 15:22:12 -0500

    added functions for displaying hangman and restarting the game

--- HANGMAN/script.js
+++ HANGMAN/script.js
@@ -11,7 +11,7 @@ const words = ['application', 'programming', 'interface', 'wizard'];
 
 let selectedWord = words[Math.floor(Math.random() * words.length)];
 
-const correctLetters = ['w', 'i', 'z', 'a', 'r', 'd'];
+const correctLetters = [];
 const wrongLetters = [];
 
 // Show hidden letter if its present in the word
@@ -36,5 +36,79 @@ function displayWord() {
     popup.style.display = 'flex';
   }
 }
+
+// Update the wrong letters
+function updateWrongLettersEl() {
+  //  Display wrong letters
+  wrongLettersEl.innerHTML = `
+  ${wrongLetters.length > 0 ? '<p>Wrong</p>' : ''}
+  ${wrongLetters.map(letter => `<span>${letter}</span>`)}
+  `;
+  // Display hangman parts
+  figureParts.forEach((part, index) => {
+    const errors = wrongLetters.length;
+
+    if (index < errors) {
+      part.style.display = 'block';
+    } else {
+      part.style.display = 'none';
+    }
+  });
+
+  //   Check if lost at game
+  if (wrongLetters.length === figureParts.length) {
+    finalMessage.innerText = 'Unfortunatly you lost.';
+    popup.style.display = 'flex';
+  }
+}
+
+// Show notification
+function showNotification() {
+  notification.classList.add('show');
+
+  setTimeout(() => {
+    notification.classList.remove('show');
+  }, 2000);
+}
+
+// Keydown letter press
+window.addEventListener('keydown', e => {
+  if (e.keyCode >= 65 && e.keyCode <= 90) {
+    const letter = e.key;
+
+    if (selectedWord.includes(letter)) {
+      if (!correctLetters.includes(letter)) {
+        correctLetters.push(letter);
+
+        displayWord();
+      } else {
+        showNotification();
+      }
+    } else {
+      if (!wrongLetters.includes(letter)) {
+        wrongLetters.push(letter);
+
+        updateWrongLettersEl();
+      } else {
+        showNotification();
+      }
+    }
+  }
+});
+
+// Restart gam and play again
+playAgainBtn.addEventListener('click', () => {
+  // Empty Arrays
+  correctLetters.splice(0);
+  wrongLetters.splice(0);
+
+  selectedWord = words[Math.floor(Math.random() * words.length)];
+
+  displayWord();
+
+  updateWrongLettersEl();
+
+  popup.style.display = 'none';
+});
 
 displayWord();
