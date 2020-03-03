dd4d41fdbe5ab45800a0349da503efb440018dd2
isaacfonsecalopez <isaacfo1010@gmail.com>
2020-03-03 17:02:56 -0500

    added next and previous song functions and add autoplay next song

--- MUSIC-PLAYER/script.js
+++ MUSIC-PLAYER/script.js
@@ -43,6 +43,51 @@ function pauseSong() {
   audio.pause();
 }
 
+// Previous song
+function prevSong() {
+  // decrement index
+  songIndex--;
+
+  if (songIndex < 0) {
+    songIndex = songs.length - 1;
+  }
+
+  loadSong(songs[songIndex]);
+
+  playSong();
+}
+
+function nextSong() {
+  // decrement index
+  songIndex++;
+
+  if (songIndex > songs.length - 1) {
+    songIndex = 0;
+  }
+
+  loadSong(songs[songIndex]);
+
+  playSong();
+}
+
+// Update progress bar
+function updateProgress(e) {
+  const { duration, currentTime } = e.srcElement;
+  const progressPercent = (currentTime / duration) * 100;
+  progress.style.width = `${progressPercent}%`;
+}
+
+// Set progress bar
+function setProgress(e) {
+  // gives you total width of bar
+  const width = this.clientWidth;
+  //   gives me the position of where i click on the bar
+  const clickX = e.offsetX;
+  const duration = audio.duration;
+
+  audio.currentTime = (clickX / width) * duration;
+}
+
 // Event Listeners
 playBtn.addEventListener('click', () => {
   const isPlaying = musicContainer.classList.contains('play');
@@ -53,3 +98,16 @@ playBtn.addEventListener('click', () => {
     playSong();
   }
 });
+
+// Change Song
+prevBtn.addEventListener('click', prevSong);
+nextBtn.addEventListener('click', nextSong);
+
+// Time/song update
+audio.addEventListener('timeupdate', updateProgress);
+
+// Click on progress bar
+progressContainer.addEventListener('click', setProgress);
+
+// Song ends and plays the next song, ended comes from audio API
+audio.addEventListener('ended', nextSong);
