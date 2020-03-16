6ec9d4554c0b92a6216bd5d633942cf845d38417
isaacfonsecalopez <isaacfo1010@gmail.com>
2020-03-16 13:09:23 -0400

    added functions for decrementing score and showing game over panel, also adding time

--- TYPING-GAME/script.js
+++ TYPING-GAME/script.js
@@ -41,6 +41,12 @@ let score = 0;
 // Init time
 let time = 10;
 
+// Focus on text on start
+text.focus();
+
+// Start counting down
+const timeInterval = setInterval(updateTime, 1000);
+
 // Generate random word from array
 function getRandomWord() {
   return words[Math.floor(Math.random() * words.length)];
@@ -58,6 +64,29 @@ function updateScore() {
   scoreEl.innerHTML = score;
 }
 
+// update time
+function updateTime() {
+  time--;
+  timeEl.innerHTML = time + 's';
+
+  if (time === 0) {
+    clearInterval(timeInterval);
+    // end game
+    gameOver();
+  }
+}
+
+// Game over, show end screen
+function gameOver() {
+  endgameEl.innerHTML = `
+    <h1> Time ran out</h1>
+    <p>Your final score is ${score}</p>
+    <button onclick="location.reload()">Reload</button>
+    `;
+
+  endgameEl.style.display = 'flex';
+}
+
 addWordTODOM();
 
 // Event listeners
@@ -70,5 +99,9 @@ text.addEventListener('input', e => {
 
     // Clear inputed text
     e.target.value = '';
+
+    time += 5;
+
+    updateTime();
   }
 });
