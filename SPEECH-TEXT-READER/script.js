049b0675171d07f969cbaef254c6924a2a6648f2
isaacfonsecalopez <isaacfo1010@gmail.com>
2020-03-16 15:43:18 -0400

    added change voice and custom text read functions

--- SPEECH-TEXT-READER/script.js
+++ SPEECH-TEXT-READER/script.js
@@ -111,6 +111,11 @@ function speakText() {
   speechSynthesis.speak(message);
 }
 
+// Set voice
+function setVoice(e) {
+  message.voice = voices.find(voice => voice.name === e.target.value);
+}
+
 // Voices changed
 speechSynthesis.addEventListener('voiceschanged', getVoices);
 
@@ -123,5 +128,14 @@ toggleBtn.addEventListener('click', () =>
 closeBtn.addEventListener('click', () =>
   document.getElementById('text-box').classList.remove('show')
 );
+
+// Change voice
+voicesSelect.addEventListener('change', setVoice);
+
+// read custom text
+readBtn.addEventListener('click', () => {
+  setTextMessage(textArea.value);
+  speakText();
+});
 
 getVoices();
