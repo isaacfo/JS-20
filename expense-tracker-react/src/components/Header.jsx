09ee6e79b7462387bee40dc1b41d8667ea22a6c4
isaacfonsecalopez <isaacfo1010@gmail.com>
2020-06-27 00:50:46 -0400

    chaged heading color

--- expense-tracker-react/src/components/Header.jsx
+++ expense-tracker-react/src/components/Header.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const Header = () => {
-  return <h2>Expense Tracker</h2>;
+  return (
+    <h2>
+      <span>Expense</span> Tracker
+    </h2>
+  );
 };
 
 export default Header;
