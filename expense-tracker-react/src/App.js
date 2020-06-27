f68bcd0a05d0b26e5d5221c4a7269ba974ebd1e7
isaacfonsecalopez <isaacfo1010@gmail.com>
2020-06-27 00:38:30 -0400

    added rest of components and added alert for empty transactions

--- expense-tracker-react/src/App.js
+++ expense-tracker-react/src/App.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import Header from './components/Header';
+import Balance from './components/Balance';
+import IncomeExpenses from './components/IncomeExpenses';
+import TransactionList from './components/TransactionList';
+import AddTransaction from './components/AddTransaction';
+import { GlobalProvider } from './context/GlobalState';
 
 import './App.css';
 
 function App() {
   return (
-    <div>
+    <GlobalProvider>
+      {/* // for other components to have access to global state, they need to be  wraped in a provider
+       */}
       <Header />
-    </div>
+      <div className="container">
+        <Balance />
+        <IncomeExpenses />
+        <TransactionList />
+        <AddTransaction />
+      </div>
+    </GlobalProvider>
   );
 }
 
